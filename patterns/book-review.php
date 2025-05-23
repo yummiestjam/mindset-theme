49645ab912fa0aa0b5e3d21138233e6aa097fa64
yummiestjam <email@email.com>
2025-05-22 19:49:37 -0700

    day 8 nearly done, before swiper install

---
+++ patterns/book-review.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Book Review
+ * Slug: mindset-theme/book-review
+ * Categories: media, text
+ */
+?>
+
+<!-- wp:media-text {"mediaId":36,"mediaLink":"http://localhost/mindset/yellow-flowers/","mediaType":"image"} -->
+<div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_theme_file_uri('assets/img/book-cover.jpg')) ?>" alt="book cover" class="wp-image-36 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:post-title /-->
+
+<!-- wp:post-author /-->
+
+<!-- wp:paragraph {"placeholder":"Content…"} -->
+<p>Description</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
